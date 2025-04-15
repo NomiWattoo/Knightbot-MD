88c44104f536b67c0ab942f6ff928c0d78ff461b
NomiWattoo <119161388+NomiWattoo@users.noreply.github.com>
2025-04-15 18:54:13 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Nomi Wattoo', // Your name
+  ownerNumber: '923701726902', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
